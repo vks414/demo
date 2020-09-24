31004d4e822b3d5a5e30e667ec8344c37be6ac57
Manoj Sharma <manoj-kumar.a.sharma@capgemini.com>
2020-09-24 14:15:46 +0530

    Initial Commit

---
+++ custom/dcxdemo/dcxdemotest/src/com/dcx/demo/test/constants/DcxdemoTestConstants.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package com.dcx.demo.test.constants;
+
+/**
+ * 
+ */
+public class DcxdemoTestConstants extends GeneratedDcxdemoTestConstants
+{
+
+	public static final String EXTENSIONNAME = "dcxdemotest";
+
+}
