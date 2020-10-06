d56067156d460e9755e8dd7bfab27406812872ee
shirish.patil-ext@rexel.com <shirish.patil-ext@rexel.com>
2020-10-06 16:59:07 +0530

    build error fixed

--- core-customize/bin/custom/dcxb2bdemo/dcxb2bdemostorefront/web/src/com/dcx/b2b/demo/storefront/controllers/misc/AnonymousConsentController.java
+++ core-customize/bin/custom/dcxb2bdemo/dcxb2bdemostorefront/web/src/com/dcx/b2b/demo/storefront/controllers/misc/AnonymousConsentController.java
@@ -72,11 +72,11 @@ public class AnonymousConsentController extends AbstractPageController
 			updatedAnonymousConsentCookie.setMaxAge((int) TimeUnit.DAYS.toSeconds(365));
 			response.addCookie(updatedAnonymousConsentCookie);
 		}
-		catch (final UnsupportedEncodingException e)
+		/*catch (final UnsupportedEncodingException e)
 		{
 			LOGGER.error("UnsupportedEncodingException occurred while decoding the Anonymous Consent Cookie", e);
 			return new ResponseEntity(HttpStatus.BAD_REQUEST);
-		}
+		}*/
 		catch (final IOException e)
 		{
 			LOGGER.error("IOException occured while reading the Anonymous Consent Cookie", e);
